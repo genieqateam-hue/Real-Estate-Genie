a2f4adcfb8c31c299b66d0b9a15cdff3295db696
sanadtalhouni97 <81582651+sanadtalhouni97@users.noreply.github.com>
2025-09-30 01:30:04 +0300

    updated pip req txt

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # Build script for Render deployment
 
+# Set Django settings module
+export DJANGO_SETTINGS_MODULE=real_estate_genie.settings
+
 # Install Python dependencies
 pip install -r requirements.txt
 
