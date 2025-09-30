6b3408ccdc408ed9cb065e8627ed3781bc91cdfb
sanadtalhouni97 <81582651+sanadtalhouni97@users.noreply.github.com>
2025-09-30 10:15:32 +0300

    update

--- build.sh
+++ build.sh
@@ -10,8 +10,17 @@ pip install -r requirements.txt
 # Run database migrations
 python manage.py migrate
 
-# Set admin password
-python manage.py set_admin_password
+# Create/update admin user with new password
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+user, created = User.objects.get_or_create(username='admin', defaults={'is_staff': True, 'is_superuser': True})
+user.set_password('Sales@2025')
+user.is_staff = True
+user.is_superuser = True
+user.save()
+print('Admin password updated to Sales@2025')
+"
 
 # Collect static files
 python manage.py collectstatic --noinput
